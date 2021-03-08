24838c5495c834d9be42b0926e5530830f8eb9f1
LiMengyang <lime0040@e.ntu.edu.sg>
2021-03-08 11:53:05 +0800

    Added Progress Bar and Created Python Run Script

--- automationtools/multiversion-fact-generation.sh
+++ automationtools/multiversion-fact-generation.sh
@@ -14,6 +14,28 @@ VERSION_DL_FILE="./Version.dl"
 VERSION_COMB_FACTS_FILE="VersionComb.facts"
 SLASH="/"
 
+# Util Function for Generating Progress Bar
+generate_progress_bar() {
+  total=$1
+  curr=$2
+  output=""
+
+  for ((i=0;i<$curr;i++))
+  do 
+    output="$output#"
+  done
+
+  for ((i=$curr;i<$total;i++))
+  do
+    output="$output."
+  done
+  percentage=$(bc <<< "scale=2;$curr/$total")
+  percentage=$(bc <<< "scale=2;$percentage*100")
+  output="$output[$percentage%]\r"
+  echo "progress bar is called"
+  echo -ne "$output"
+}
+
 # Step 0: Read command line input
 while [[ "$#" -gt 0 ]]; do
   case $1 in
@@ -55,11 +77,17 @@ souffle -F ${OUTPUT_PATH}${SLASH}${FACTS_OUTPUT_REL_PATH} ${VERSION_DL_FILE}
 # Step 2: Check out each of the unique commit
 cd ${REPO_PATH}
 ORIGINAL_COMMIT=$(git rev-parse HEAD)
+LENGTH=$(wc -l /Users/limengyang/Workspaces/FinalYearProject/codeqltosouffle/automationtools/run.py)
+LENGTH=($LENGTH)
+LENGTH=${LENGTH[0]}
+CURR_COUNT=1
 for commit in $(${CODEQLTOSOUFFLE_PATH}${SLASH}${GET_UNIQUE_COMMITS_FROM_FACTS_FILE_REL_PATH} ${OUTPUT_PATH}${SLASH}${FACTS_OUTPUT_REL_PATH}${SLASH}${VERSION_COMB_FACTS_FILE}); do
-    echo ${commit}
+    generate_progress_bar $LENGTH $CURR_COUNT
+    wait $!
     git checkout ${commit}
     ${CODEQLTOSOUFFLE_PATH}${SLASH}${FACT_GENERATION_REL_PATH} --repo_path ${REPO_PATH} --cslicer_path ${CSLICER_PATH} --output_path ${OUTPUT_PATH} --commit ${commit}
     wait $!
+    ((CURR_COUNT++))
 done
 git checkout ${ORIGINAL_COMMIT}
 
---
+++ automationtools/run.py
@@ -0,0 +1,115 @@
+import argparse
+import os
+import sys
+import subprocess
+from subprocess import check_output
+import psutil
+from pyfiglet import Figlet
+
+def validatePath(path):
+    if ((not os.path.isdir(path)) and (not os.path.isfile(path))):
+        print('The path or file ' + path + ' specified does not exist')
+        sys.exit()
+
+# Print name of the APP
+title = Figlet(font='slant')
+subtitle = Figlet(font='bubble')
+print(title.renderText('CodeVQL'))
+print(subtitle.renderText('Version-Aware Security Analysis'))
+
+# Create the parser
+runner = argparse.ArgumentParser(description='\
+            This is the Command Line Interface(CLI) of the CodeVQL language. \
+            CodeVQL can perform the basic security analysis in a version-aware way. \
+            For more information, please check at this link [TODO]. \
+        ')
+
+# Add the arguments
+runner.add_argument('--repo_path',
+                    type=str,
+                    help='the path to the program to be analyzed upon')
+runner.add_argument('--gitfacts_path',
+                    type=str,
+                    help='the path to the tool that generates git facts, install here(https://github.com/uxhg/ext-gitfacts)')
+runner.add_argument('--output_path',
+                    type=str,
+                    help='the path to the analysis output')   
+runner.add_argument('--query_file_path',
+                    type=str,
+                    help='the path to the input query file')
+runner.add_argument('--codeqltosouffle_path',
+                    type=str,
+                    help='the path to the translator from query langauge to internal declarative langauge, install here(https://github.com/LiMengyang990726/codeqltosouffle/)')
+runner.add_argument('--cslicer_path',
+                    type=str,
+                    help='the path to the tool that generates program facts, install here(https://bitbucket.org/liyistc/gitslice/src/facts-dl4ql/)')            
+
+# Get all the input arguments and validate
+args = runner.parse_args()
+
+repo_path = args.repo_path
+validatePath(repo_path)
+
+gitfacts_path = args.gitfacts_path
+validatePath(gitfacts_path)
+
+output_path = args.output_path
+subprocess.run(['mkdir', '-p', output_path])
+subprocess.run(['chmod', '-R', '777', output_path])
+validatePath(output_path)
+
+query_file_path = args.query_file_path
+validatePath(query_file_path)
+
+codeqltosouffle_path = args.codeqltosouffle_path
+validatePath(codeqltosouffle_path)
+
+cslicer_path = args.cslicer_path
+validatePath(cslicer_path)
+
+# Step 1: Generate Git Facts
+process = subprocess.Popen(
+    'echo Step 1: Start generating git facts && \
+    %s/automationtools/gitfact-generation.sh --repo_path %s --gitfacts_path %s --output_path %s && \
+    echo Step 1 is done!' % (codeqltosouffle_path, repo_path, gitfacts_path, output_path), 
+    shell=True
+)
+# subprocess.Popen(
+#     '[TODO]' % process.pid,
+#     shell=True
+# )
+process.wait()
+
+# Step 2: Translate CodeVQL to Souffle
+process = subprocess.Popen(
+    'echo Step 2: Start translating CodeVQL to Souffle && \
+    %s/automationtools/translate.sh --query_file_path %s --codeqltosouffle_path %s --output_path %s && \
+    echo Step 2 is done!' % (codeqltosouffle_path, query_file_path, codeqltosouffle_path, output_path),
+    shell=True
+)
+process.wait()
+
+# Step 3: Get multiversion facts
+process = subprocess.Popen(
+    'echo Step 3: Start Generating program facts in selected version && \
+    %s/automationtools/multiversion-fact-generation.sh --repo_path %s --cslicer_path %s --codeqltosouffle_path %s --output_path %s > /dev/null 2>&1 && \
+    echo Step 3 is done!' % (codeqltosouffle_path, repo_path, cslicer_path, codeqltosouffle_path, output_path),
+    shell=True
+)
+process.wait()
+
+# Step 4: Analyze to get the result
+process = subprocess.Popen(
+    'echo Step 4: Start analyzing the query && \
+    mkdir %s/output && souffle -F %s/.facts/20-deps/ -D %s/output %s/rules/main.dl && \
+    echo Step 4 is done and program finished!' % (output_path, output_path, output_path, output_path),
+    shell=True
+)
+process.wait()
+
+# python3.7 run.py --repo_path /Users/limengyang/Workspaces/FinalYearProject/docker-maven-plugin \
+#          --gitfacts_path /Users/limengyang/Workspaces/FinalYearProject/ext-gitfacts \
+#          --output_path /Users/limengyang/Desktop/test4 \
+#          --query_file_path /Users/limengyang/Workspaces/FinalYearProject/codeqltosouffle/tests/useCaseQueries/methodUnused.txt \
+#          --codeqltosouffle_path /Users/limengyang/Workspaces/FinalYearProject/codeqltosouffle \
+#          --cslicer_path /Users/limengyang/Workspaces/FinalYearProject/gitslice/target/cslicer-1.0.0-jar-with-dependencies.jar
