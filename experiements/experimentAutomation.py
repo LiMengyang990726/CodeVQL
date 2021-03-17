50bf4edcafee94854656d8f57d611736978f9eca
LiMengyang990726 <mengyang0726@gmail.com>
2021-03-17 10:20:16 +0800

    Updated experiment automation script

--- experiements/experimentAutomation.py
+++ experiements/experimentAutomation.py
@@ -11,15 +11,19 @@ log_file = "/home/mengyang/FYP/experiments/log.txt"
 
 for path in Path('./').rglob('*.txt'):
     repo_path_rel = str(path.parents[1])
-    output_path_rel = os.path.join(str(path.parents[1]), os.path.splitext(path)[0] + str(path.parents[0])[-1])
+    output_path_rel = os.path.join(str(path.parents[1]), os.path.splitext(path.name)[0] + str(path.parents[0])[-1])
     query_file_path_rel = os.path.join(str(path.parents[0]), path.name)
     repo_path = os.path.join(repo_path_abs, repo_path_rel)
     output_path = os.path.join(output_path_abs, output_path_rel)
     query_file_path = os.path.join(query_file_path_abs, query_file_path_rel)
-    command = "python3.7 run.py --repo_path %s \
+    command = "python3.7 ../automationtools_py/run.py --repo_path %s \
          --gitfacts_path %s \
          --output_path %s \
          --query_file_path %s \
          --codeqltosouffle_path %s \
          --cslicer_path %s >> %s" % (repo_path, gitfacts_path, output_path, query_file_path, codeqltosouffle_path, cslicer_path, log_file)
+    f = open(log_file, "a")
+    f.write(command)
+    f.write("\n")
+    f.close()
     os.system(command)
